07a7e670cb854ecbf3b17eb8891e971781a30e53
jungchaen <yllowrose452@naver.com>
2022-10-07 17:33:47 +0900

    곱하기를 커밋함.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+[
++Test/src/main/java/com/nhnacademy/Main.java,6\0\60a3d043e55a4981f1b00cf83b5129bff643174c
+_
+/Test/src/main/java/com/nhnacademy/Multiply.java,5\4\541a7715ad9de7b3cc6008969350322980f4e752
--- Test/src/main/java/com/nhnacademy/Main.java
+++ Test/src/main/java/com/nhnacademy/Main.java
@@ -3,5 +3,8 @@ package com.nhnacademy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("계산기 만들어주세요!! - 승혜: 더하기 , 승아: 나머지, 채은: 곱하기 , 동빈: 나누기 ");
+
+        Multiply mul = new Multiply();
+        System.out.println(mul.mulitply(3, 5));
     }
 }
