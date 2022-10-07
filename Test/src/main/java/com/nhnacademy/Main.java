af4054ce1b67117187c68d827113279167ef1fa3
seohyeon <kgustjgus@naver.com>
2022-10-07 17:18:25 +0900

    first commit

---
+++ Test/src/main/java/com/nhnacademy/Main.java
@@ -0,0 +1,7 @@
+package com.nhnacademy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("계산기 만들어주세요!! - 승혜: 더하기 , 승아: 나머지, 채은: 곱하기 , 동빈: 나누기 ");
+    }
+}
